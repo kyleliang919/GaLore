d17a76ab000316781befdb492e947df847d916a1
Kaizhao Liang <kyleliang919@gmail.com>
2024-05-12 08:55:02 -0500

    Update __init__.py

--- galore_torch/__init__.py
+++ galore_torch/__init__.py
@@ -1,3 +1,4 @@
 from .adafactor import Adafactor as GaLoreAdafactor
 from .adamw import AdamW as GaLoreAdamW
 from .adamw8bit import AdamW8bit as GaLoreAdamW8bit
+from .lion import Lion, GaLoreLion 
